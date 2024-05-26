5bfc570ca121f237648c1b1c1c8825d6b0aa6274
Sofia Nguyen <120623425+symsoph@users.noreply.github.com>
2024-05-25 23:36:49 -0700

    scope of sw.js - specific to gh pages

--- assets/scripts/main.js
+++ assets/scripts/main.js
@@ -51,7 +51,7 @@ function initializeServiceWorker() {
       // Steps B3-B6 will be *inside* the event listener's function created in B2
       // B3. TODO - Register './sw.js' as a service worker (The MDN article
       //            "Using Service Workers" will help you here)
-      const registration = await navigator.serviceWorker.register("./sw.js", {
+      const registration = await navigator.serviceWorker.register("./Lab8-Starter/blob/main/sw.js", {
         scope: "./",
       });
       // B4. TODO - Once the service worker has been successfully registered, console
