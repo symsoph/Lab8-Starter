9216007f821cc503b6f00acd652b9b5f3e7b631b
Sofia Nguyen <120623425+symsoph@users.noreply.github.com>
2024-05-25 22:17:37 -0700

    finished part 2

--- README.md
+++ README.md
@@ -1,1 +1,6 @@
 # Lab8-Starter
+
+Name: Sofia Nguyen
+
+How are graceful degradation and service workers related?
+- Service workers are related to graceful degradation because they allow newer versions of a web app (like having internet) to have the best experiences while older versions (browsers without internet) to still have core functionalities of the same web app.  We were still able to see the recipe cards while being offline.  We create a fallback experience with service workers and thus are following the principles of graceful degradation.  
--- sw.js
+++ sw.js
@@ -3,13 +3,24 @@
 
 const CACHE_NAME = 'lab-8-starter';
 
+const RECIPE_URLS = [
+  'https://adarsh249.github.io/Lab8-Starter/recipes/1_50-thanksgiving-side-dishes.json',
+  'https://adarsh249.github.io/Lab8-Starter/recipes/2_roasting-turkey-breast-with-stuffing.json',
+  'https://adarsh249.github.io/Lab8-Starter/recipes/3_moms-cornbread-stuffing.json',
+  'https://adarsh249.github.io/Lab8-Starter/recipes/4_50-indulgent-thanksgiving-side-dishes-for-any-holiday-gathering.json',
+  'https://adarsh249.github.io/Lab8-Starter/recipes/5_healthy-thanksgiving-recipe-crockpot-turkey-breast.json',
+  'https://adarsh249.github.io/Lab8-Starter/recipes/6_one-pot-thanksgiving-dinner.json',
+];
+
+
 // Installs the service worker. Feed it some initial URLs to cache
 self.addEventListener('install', function (event) {
   event.waitUntil(
     caches.open(CACHE_NAME).then(function (cache) {
       // B6. TODO - Add all of the URLs from RECIPE_URLs here so that they are
       //            added to the cache when the ServiceWorker is installed
-      return cache.addAll([]);
+      // https://developer.chrome.com/docs/workbox/caching-strategies-overview/
+      return cache.addAll(RECIPE_URLS);
     })
   );
 });
@@ -32,9 +43,30 @@ self.addEventListener('fetch', function (event) {
   //       fetch(event.request)
   // https://developer.chrome.com/docs/workbox/caching-strategies-overview/
   /*******************************/
+  
+  
   // B7. TODO - Respond to the event by opening the cache using the name we gave
   //            above (CACHE_NAME)
-  // B8. TODO - If the request is in the cache, return with the cached version.
-  //            Otherwise fetch the resource, add it to the cache, and return
-  //            network response.
-});
+  event.respondWith(caches.open(CACHE_NAME).then((cache) => {
+    // B8. TODO - If the request is in the cache, return with the cached version.
+    // Go to the cache first
+    return cache.match(event.request.url).then((cachedResponse) => {
+      // Return a cached response if we have one
+      if (cachedResponse) {
+        return cachedResponse;
+      }
+      
+      //            Otherwise fetch the resource, add it to the cache, and return
+      //            network response.
+        // Otherwise, hit the network
+        return fetch(event.request).then((fetchedResponse) => {
+          // Add the network response to the cache for later visits
+          cache.put(event.request, fetchedResponse.clone());
+
+          // Return the network response
+          return fetchedResponse;
+        });
+      });
+    }));
+
+  });
